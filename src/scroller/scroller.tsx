749077cd1e43b82b553e307196481e3527937376
Max Nedelko <59868097+maxweek@users.noreply.github.com>
2024-04-15 17:52:17 +0300

    fixes

--- src/scroller/scroller.tsx
+++ src/scroller/scroller.tsx
@@ -1,53 +1,8 @@
 import React, { FC, ReactNode, WheelEventHandler, forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
 import "./styles.scss"
 import { getCl, getClR } from "./helper";
+import { IScroller, IScrollerProperties, IScrollerRef } from ".";
 
-export interface IScroller {
-  children: ReactNode,
-  needBar?: boolean,
-  barAltPosition?: boolean
-  horizontal?: boolean
-  grab?: boolean
-  borderFade?: boolean,
-  borderPadding?: boolean,
-  grabCursor?: boolean
-  className?: string,
-  barClassName?: string,
-  barRollerClassName?: string,
-  contentClassName?: string,
-  onReachStart?: () => void;
-  onReachEnd?: () => void;
-  onScroll?: () => void;
-}
-
-export interface IScrollerProperties {
-  height: number,
-  top: number,
-  boxHeight: number,
-  progress: number,
-  grab: boolean,
-  grabOffset: number,
-  grabStart: number,
-  grabDelta: number,
-  scrollStart: number,
-  hovered: boolean,
-  bar: {
-    height: number,
-    offset: number,
-    offsetStart: number,
-    offsetDelta: number,
-    clicked: boolean
-  }
-}
-
-export interface IScrollerRef {
-  scrollTo: (offset: number) => void;
-  scrollToStart: () => void;
-  scrollToEnd: () => void;
-  update: () => void;
-  getProperties: () => IScrollerProperties;
-  scrollRef: React.RefObject<HTMLDivElement>;
-}
 
 export const Scroller = forwardRef<IScrollerRef, IScroller>((props: IScroller, ref) => {
   const SCROLL = useRef<IScrollerProperties>({
