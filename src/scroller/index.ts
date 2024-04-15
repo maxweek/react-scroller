b72c5b41422cdb9709f114a647432ec98c72c56a
Max Nedelko <59868097+maxweek@users.noreply.github.com>
2024-04-15 17:51:46 +0300

    fixes

--- src/scroller/index.ts
+++ src/scroller/index.ts
@@ -1,3 +1,52 @@
+import React, { ReactNode } from "react";
 import { Scroller } from "./scroller";
+
+
+export interface IScroller {
+    children: ReactNode,
+    needBar?: boolean,
+    barAltPosition?: boolean
+    horizontal?: boolean
+    grab?: boolean
+    borderFade?: boolean,
+    borderPadding?: boolean,
+    grabCursor?: boolean
+    className?: string,
+    barClassName?: string,
+    barRollerClassName?: string,
+    contentClassName?: string,
+    onReachStart?: () => void;
+    onReachEnd?: () => void;
+    onScroll?: () => void;
+}
+
+export interface IScrollerProperties {
+    height: number,
+    top: number,
+    boxHeight: number,
+    progress: number,
+    grab: boolean,
+    grabOffset: number,
+    grabStart: number,
+    grabDelta: number,
+    scrollStart: number,
+    hovered: boolean,
+    bar: {
+        height: number,
+        offset: number,
+        offsetStart: number,
+        offsetDelta: number,
+        clicked: boolean
+    }
+}
+
+export interface IScrollerRef {
+    scrollTo: (offset: number) => void;
+    scrollToStart: () => void;
+    scrollToEnd: () => void;
+    update: () => void;
+    getProperties: () => IScrollerProperties;
+    scrollRef: React.RefObject<HTMLDivElement>;
+}
 
 export default Scroller
